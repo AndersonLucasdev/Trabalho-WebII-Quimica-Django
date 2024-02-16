39ad8e514bf2719011900c28ed9b3ac0bea48cfb
anderson <andinlucas8@gmail.com>
2024-02-16 19:56:05 -0300

    feat/ forms: Adicionando os campos de amostra e criando AmostraForm

--- src/quimica/models.py
+++ src/quimica/models.py
@@ -5,4 +5,4 @@ from django.db import models
 class Amostra(models.Model):
     nome = models.CharField(max_length=100)
     descricao = models.TextField()
-    qntd_solucao = models.FloatField()()
+    quantidade_solucao = models.FloatField()()
