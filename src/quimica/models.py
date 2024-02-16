bae0f6b28518254d92fb4a77acd4c9a7f103520b
anderson <andinlucas8@gmail.com>
2024-02-15 22:43:22 -0300

    feat/ models: Criando o modelo com os campos da amostra

--- src/quimica/models.py
+++ src/quimica/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Amostra(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+    qntd_solucao = models.FloatField()()
