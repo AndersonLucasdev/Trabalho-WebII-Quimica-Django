065644331e0cba1d251a7f1ff055f86e883f1bb8
anderson <andinlucas8@gmail.com>
2024-02-17 11:07:16 -0300

    feat/ views: Feita a views de criar amostra

--- src/amostras/views.py
+++ src/amostras/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render, redirect
 from .forms import AmostraForm
 # Create your views here.
 
+def criar_amostra(request):
+    if request.method == 'POST':
+        form = AmostraForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('mostrar_amostras', amostra_id = form.instance.id)
+        else:
+            form = AmostraForm()
+    return render(request, 'amostras/criar_amostra.html', {'form': form})
