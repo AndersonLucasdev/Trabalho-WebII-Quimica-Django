fe12d382e2465a6b7721af77caf24c239ef11eee
anderson <andinlucas8@gmail.com>
2024-02-17 10:56:06 -0300

    feat/ views: Alterando importações e estrutura

--- src/amostras/views.py
+++ src/amostras/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import AmostraForm
 # Create your views here.
 
-def index(request):
-    return HttpResponse("Inicando os trabalhos!")
