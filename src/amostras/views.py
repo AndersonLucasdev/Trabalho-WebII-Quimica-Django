7620ca3059b6a1c0cf21efd8f3c67142fdf684d0
anderson <andinlucas8@gmail.com>
2024-02-18 09:13:05 -0300

    feat/ views: Criando página para mostrar as amostras

--- src/amostras/views.py
+++ src/amostras/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import AmostraForm
+from .forms import Amostra
 # Create your views here.
+
+def mostrar_amostras(request):
+    amostras = get_object_or_404(Amostra)
+    return render(request, 'amostras/mostrar_amostras.html', {'amostras': amostras})
+
+
 
 def criar_amostra(request):
     if request.method == 'POST':
