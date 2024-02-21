40aa1c9beb813fc00c9da501bf4ab1eb12e85469
anderson <andinlucas8@gmail.com>
2024-02-21 17:37:13 -0300

    fix/ Resolvendo problemas de pacotes

--- src/amostras/views.py
+++ src/amostras/views.py
@@ -3,11 +3,12 @@ from .forms import AmostraForm
 from .forms import Amostra
 # Create your views here.
 
+def index():
+    return render("Ola mundo!")
+
 def detalhes_das_amostras(request):
     amostras = Amostra.objects.all()
     return render(request, 'amostras/detalhes_das_amostras.html', {'amostras': amostras})
-
-
 
 def criar_amostra(request):
     if request.method == 'POST':
