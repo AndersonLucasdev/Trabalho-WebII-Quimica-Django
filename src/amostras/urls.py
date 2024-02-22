5c54aeb66155cc7e3afe381401e72ddb4e75a027
anderson <andinlucas8@gmail.com>
2024-02-22 16:45:42 -0300

    feat/ urls: Adicionando as urls de cadastro e visualização

--- src/amostras/urls.py
+++ src/amostras/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path('cadastrar/', views.criar_amostra, name='cadastrar_amostra'),
+    path('mostrar/', views.detalhes_das_amostras, name='mostrar_amostras')
 ]
