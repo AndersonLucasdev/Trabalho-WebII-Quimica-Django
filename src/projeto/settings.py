dbcfdacb83c142c068db04496ecd198d852e716a
anderson <andinlucas8@gmail.com>
2024-03-03 20:28:13 -0300

    feat/ setting: Ajustando configurações de auth

--- src/projeto/settings.py
+++ src/projeto/settings.py
@@ -40,6 +40,12 @@ INSTALLED_APPS = [
     'amostras',
 ]
 
+## auth
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
